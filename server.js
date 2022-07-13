42f045653def6295672e42d2a61be2c076a581ba
Strawhorse <strawhorse@gmail.com>
2022-07-13 17:53:28 +0100

    POST request added

--- server.js
+++ server.js
@@ -4,23 +4,55 @@ const PORT = 3000
 
 const friends = [
     {
-        id: 1,
+        id: 0,
         Name: "John"
     },
     {
-        id:2,
+        id: 1,
         Name: "JinJing"
     },
     {
-        id: 3,
+        id: 2,
         Name: "Virgil"
     }
 ]
 
-app.get('/', (req,res) => {
+// add middleware to register how long request took
+app.use((req,res,next) => {
+    const start = Date.now()
+    next();
+    const delta = Date.now() - start
+    console.log(`${req.method} ${req.url} ${delta}ms`)
+    // always call next() function or middleware will not release request and it will time out
+})
+
+// middleware which looks at Content-Type and parses it if it is json, so can then be added to our list of contacts
+app.use(express.json())
+
+// route for posting to the friends address
+app.post('/friends', (req,res) => {
+    if (!req.body.name) {
+        return res.status(400).json({
+            Error: "No friend name included in request"
+        })
+    } else {
+        const newFriend = {
+            id: friends.length,
+            name: req.body.name
+        }
+        friends.push(newFriend)
+        res.status(200).json(newFriend)
+        console.log(friends)
+    }
+})
+
+// route for getting the full list of friends
+app.get('/friends', (req,res) => {
+    console.log(friends)
     res.json(friends)
 })
 
+// route for getting a specific friend based upon the parameter entered after : in the parsed address
 app.get('/friends/:friendID', (req,res)=>{
     // after the : above, Express will take whatever parameter comes here as the location to search
     const friendID = Number(req.params.friendID)
@@ -29,22 +61,12 @@ app.get('/friends/:friendID', (req,res)=>{
         res.status(200).json(friend)
     } else {
         res.status(404).json({
-            error: "Friend at this address does not exist"
+            error: 'Friend at this address does not exist'
         });
     }
 })
 
-app.get('/message', (req,res) => {
-    res.send('<ul>Hi there in HTML languages</ul>')
-})
-
-app.post('/message', (req,res) => {
-    console.log("Updating messages...")
-})
-
-
-
-
+// set listening to port
 app.listen(PORT, () => {
     console.log(`Server is listening on port: ${PORT}`)
 })
